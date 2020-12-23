c04039dddf291fc035b3d0c97d4579228c47ef5e
BihaiGuo <bihai-guo@msyc.cc>
2020-12-23 22:45:54 +0800

    测试路由传值

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,43 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  //跳转传值
+  void navToPage(BuildContext context) {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          return TabOrderInfo();
+        },
+        settings: RouteSettings(
+          arguments: {'name': 'postbird'},
+        ), // 传参
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    //接收路由转值
+    Map args = ModalRoute.of(context).settings.arguments;
+    if (null != args) {
+      if (args.containsKey("name")) {
+        String name = args["name"];
+      }
+    }
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: Colors.red,
       ),
       home: TabOrderInfo(),
+      routes: {
+        //路由跳转
+        'first': (context) {
+          return TabOrderInfo();
+        }
+      },
     );
   }
 }
